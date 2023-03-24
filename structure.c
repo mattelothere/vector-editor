608f120448bfe295634f327a93203b9a634ed046
Zongoula <75811319+campoceblo@users.noreply.github.com>
2023-03-24 13:25:38 +0100

    Creation de toutes les structs saufe polygon, probleme concernant l'array de point

--- structure.c
+++ structure.c
@@ -30,3 +30,56 @@ void delete_line(Line * line){
 void print_line(Line * line){
     printf("LINE %d %d %d %d", line->p1->pos_x, line->p1->pos_y, line->p2->pos_x, line->p2->pos_y);
 }
+
+Square *create_square(Point * point, int length){
+    Square *square = (Square *) malloc(sizeof (Square));
+    square->p = point;
+    square->lenght = length;
+    return square;
+}
+void delete_square(Square * square){
+    free(square);
+}
+void print_square(Square * square) {
+    printf("SQUARE %d %d %d", square->p->pos_x, square->p->pos_y, square->lenght);
+}
+
+Rectangle *create_rectangle(Point * point, int width, int height){
+    Rectangle *rectangle = (Rectangle *) malloc(sizeof (Rectangle));
+    rectangle->p = point;
+    rectangle->lenght = height;
+    rectangle->width = width;
+    return rectangle;
+}
+void delete_rectangle(Rectangle * rectangle){
+    free(rectangle);
+}
+void print_rectangle(Rectangle * rectangle){
+    printf("RECTANGLE x =%d y =%d Height: %d Width: %d\n", rectangle->p->pos_x, rectangle->p->pos_y, rectangle->lenght, rectangle->width);
+}
+
+
+Circle *create_circle(Point * center, int radius){
+    Circle *circle = (Circle *) malloc(sizeof (Circle));
+    circle->p = center;
+    circle->radius = radius;
+}
+void delete_circle(Circle * circle){
+    free(circle);
+}
+void print_circle(Circle * circle){
+    printf("CIRCLE x = %d y = %d radius = %d\n", circle->p->pos_x, circle->p->pos_y, circle->radius);
+}
+
+
+Polygon *create_polygon(int n){
+    Polygon * polygon = (Polygon *) malloc(sizeof (Polygon));
+    polygon->n = n;
+
+}
+void delete_polygon(Polygon * polygon){
+    free(polygon);
+}
+void print_polygon(Polygon * polygon){
+    printf("POLYGON")
+}
--- structure.h
+++ structure.h
@@ -11,8 +11,27 @@ typedef struct line {
     Point *p2;
 }Line;
 
+typedef struct square {
+    Point *p;
+    int lenght;
 
+}Square;
 
+typedef struct rectangle {
+    Point *p;
+    int lenght;
+    int width;
+}Rectangle;
+
+typedef struct circle {
+    Point *p;
+    int radius;
+}Circle;
+
+typedef struct polygon {
+    int n;
+    Point ** points;
+}Polygon;
 
 Point *create_point(int px, int py);
 void delete_point(Point * point);
@@ -22,5 +41,20 @@ Line *create_line(Point * p1, Point * p2);
 void delete_line(Line * line);
 void print_line(Line * line);
 
+Square *create_square(Point * point, int length);
+void delete_square(Square * square);
+void print_square(Square * square);
+
+Rectangle *create_rectangle(Point * point, int width, int height);
+void delete_rectangle(Rectangle * rectangle);
+void print_rectangle(Rectangle * rectangle);
+
+Circle *create_circle(Point * center, int radius);
+void delete_circle(Circle * circle);
+void print_circle(Circle * circle);
+
+Polygon *create_polygon(int n);
+void delete_polygon(Polygon * polygon);
+void print_polygon(Polygon * polygon);
 
 #endif
